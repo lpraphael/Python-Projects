371a573d44c197b8d75a7004b088708c9965a9c4
lpraphael <raphael.perazzo@gmail.com>
2022-11-09 13:04:47 -0300

    feat: implementing error handling when dividing by 0

--- project-honest-calculator/honest-calculator.py
+++ project-honest-calculator/honest-calculator.py
@@ -2,6 +2,7 @@ def honest_calculator():
     MSG_0 = "Enter an equation"
     MSG_1 = "Do you even know what numbers are? Stay focused!"
     MSG_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+    MSG_3 = "Yeah... division by zero. Smart move..."
 
     print(MSG_0)
     equation = input()
@@ -19,11 +20,14 @@ def honest_calculator():
         elif oper == '*':
             result = x * y
         elif oper == '/':
+            if x == 0 or y == 0:
+                raise ZeroDivisionError
             result = x * y
+            
         else:
             raise SyntaxError
 
-        return result
+        return print(result)
 
     except (TypeError, ValueError):
         print(MSG_1)
@@ -32,6 +36,9 @@ def honest_calculator():
     except SyntaxError:
         print(MSG_2)
         honest_calculator()
-
+    except ZeroDivisionError:
+        print(MSG_3)
+        honest_calculator()
+        
 
 honest_calculator()
