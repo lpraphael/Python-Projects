30e6509c36e701da831ad939718608882902b786
lpraphael <raphael.perazzo@gmail.com>
2022-11-10 22:58:48 -0300

    feat: implementando novas funcionalidades

--- project-honest-calculator/honest-calculator.py
+++ project-honest-calculator/honest-calculator.py
@@ -1,44 +1,71 @@
 def honest_calculator():
     MSG_0 = "Enter an equation"
     MSG_1 = "Do you even know what numbers are? Stay focused!"
-    MSG_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
-    MSG_3 = "Yeah... division by zero. Smart move..."
+    MSG_2 = '''Yes, an interesting math operation.
+            You've slept through all classes, haven't you?'''
+    MSG_3 = "Yeah, division by zero. Smart move..."
+    MSG_4 = "Do you want to store the result? (y / n):"
+    MSG_5 = "Do you want to continue calculations? (y / n):"
+    memory = 0
+    result = "y"
 
-    print(MSG_0)
-    equation = input()
+    while result == "y":
 
-    x, oper, y = equation.split()
+        print(MSG_0)
+        equation = input()
 
-    try:
-        x = float(x)
-        y = float(y)
+        x, oper, y = equation.split()
 
-        if oper == "+":
-            result = x + y
-        elif oper == "-":
-            result = x - y
-        elif oper == "*":
-            result = x * y
-        elif oper == "/":
-            if x == 0 or y == 0:
-                raise ZeroDivisionError
-            result = x * y
+        if x == "M":
+            x = memory
 
-        else:
-            raise SyntaxError
+        if y == "M":
+            y = memory
 
-        return print(result)
+        try:
+            x = float(x)
+            y = float(y)
 
-    except (TypeError, ValueError):
-        print(MSG_1)
-        honest_calculator()
+            if oper == "+":
+                result = x + y
+            elif oper == "-":
+                result = x - y
+            elif oper == "*":
+                result = x * y
+            elif oper == "/":
+                if x == 0 or y == 0:
+                    raise ZeroDivisionError
+                result = x * y
 
-    except SyntaxError:
-        print(MSG_2)
-        honest_calculator()
-    except ZeroDivisionError:
-        print(MSG_3)
-        honest_calculator()
+            else:
+                raise SyntaxError
+
+            print(result)
+
+            print(MSG_4)
+
+            answer_1 = input()
+
+            if answer_1 == "y":
+                memory = result
+
+            print(MSG_5)
+
+            answer_2 = input()
+
+            result = answer_2
+
+        except (TypeError, ValueError):
+            print(MSG_1)
+            result = "y"
+        except SyntaxError:
+            print(MSG_2)
+            result = "y"
+        except ZeroDivisionError:
+            print(MSG_3)
+            result = "y"
+
+    return None
 
 
 honest_calculator()
