c621f8ae18f48796442ea38a565691d9018470a6
lpraphael <raphael.perazzo@gmail.com>
2022-11-10 23:01:05 -0300

    feat: implementando novas funcionalidades

--- project-honest-calculator/honest-calculator.py
+++ project-honest-calculator/honest-calculator.py
@@ -1,12 +1,13 @@
 def honest_calculator():
     MSG_0 = "Enter an equation"
     MSG_1 = "Do you even know what numbers are? Stay focused!"
-    MSG_2 = '''Yes, an interesting math operation.
-            You've slept through all classes, haven't you?'''
+    MSG_2 = """Yes, an interesting math operation.
+            You've slept through all classes, haven't you?"""
     MSG_3 = "Yeah, division by zero. Smart move..."
     MSG_4 = "Do you want to store the result? (y / n):"
     MSG_5 = "Do you want to continue calculations? (y / n):"
-    memory = 0
+
+    memory = 0.0
     result = "y"
 
     while result == "y":
