c82521bf5a1d07624d7b7301298c137f7382c2d2
lpraphael <raphael.perazzo@gmail.com>
2022-12-09 16:15:06 -0300

    feat: Implementing a simple gameplay where the user needs to guess the secret word.

--- project-hangman/hangman.py
+++ project-hangman/hangman.py
@@ -1,10 +1,14 @@
 def hangman():
-    print(
-        """
-        H A N G M A N"
-        The game will be available soon.
-        """
-    )
+    print('H A N G M A N')
+
+    secret_word = 'python'
+
+    player_word = input('Guess the word: ')
+
+    if player_word == secret_word:
+        return print('You survived!')
+    else:
+        return print('You lost!')
 
 
 hangman()
