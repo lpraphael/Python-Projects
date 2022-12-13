fc10f3d7bccee387d7928e6cdb9322aacd0be3fd
lpraphael <raphael.perazzo@gmail.com>
2022-12-13 00:10:20 -0300

    feat: decreasing the complexity to guess the word

--- project-hangman/hangman.py
+++ project-hangman/hangman.py
@@ -1,14 +1,24 @@
+import random
+
+
 def hangman():
-    print('H A N G M A N')
+    print("H A N G M A N")
 
-    secret_word = 'python'
+    words = ("python", "java", "swift", "javascript")
 
-    player_word = input('Guess the word: ')
+    secret_word = random.choice(words)
+
+    new_word = secret_word[0:3]
+
+    for _ in range(3, len(secret_word)):
+        new_word += "-"
+
+    player_word = input(f"Guess the word {new_word}: ")
 
     if player_word == secret_word:
-        return print('You survived!')
+        return print("You survived!")
     else:
-        return print('You lost!')
+        return print("You lost!")
 
 
 hangman()
